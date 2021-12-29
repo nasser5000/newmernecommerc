16ecb2d76c80ebe587e34ebf20d2cfde23a7a465
Nasser Salem <nassersalemf5000@gmail.com>
2021-12-29 19:57:47 +0200

    Add A new products to DB
    - create a new product function
    - test new product function with postman
    - delete the test db productadd bulke product with seeder

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
-const path = require('path');
 const app = require('./app');
+const connectDatabase = require("./config/database");
+
 const dotenv = require('dotenv');
+
 
 //Setting up configuration file path
 dotenv.config({path: './backend/config/config.env'})
+
+//connected to Database instance
+connectDatabase();
+
 app.listen(process.env.PORT , ()=>{
     console.log(`Server Started on PORT : ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 })
